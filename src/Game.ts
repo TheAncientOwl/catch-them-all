059f8242d3ad6975297cca7861eb44a0ee33419f
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-06-05 10:58:18 +0300

    refactoring

--- src/Game.ts
+++ src/Game.ts
@@ -1,26 +1,31 @@
 import * as THREE from 'three';
 
-import SceneRenderer from './managers/SceneRenderer';
-import LightingManager from './managers/LightingManager';
-import Player from './objects/Player';
-import InputManager from './managers/InputManager';
 import Timer from './utilities/Timer';
+import Player from './objects/Player';
 import Ground from './objects/Ground';
-import PokeballSpawner from './managers/PokeballSpawner';
 import TimeManager from './managers/TimeManager';
+import InputManager from './managers/InputManager';
+import SceneRenderer from './managers/SceneRenderer';
+import PokeballSpawner from './managers/PokeballSpawner';
+import LightingManager from './managers/LightingManager';
 
 export default class Game {
+  private timer: Timer;
+
   private sceneRenderer: SceneRenderer;
   private lightingManager: LightingManager;
+
   private inputManager: InputManager;
   private fruitSpawner: PokeballSpawner;
-  private timer: Timer;
-  private player: Player;
   private ground: Ground;
+  private player: Player;
+
   private paused: boolean;
   private pauseHTMLElement: HTMLElement;
 
   public constructor() {
+    this.timer = new Timer();
+
     this.sceneRenderer = new SceneRenderer();
 
     this.lightingManager = new LightingManager();
@@ -28,7 +33,6 @@ export default class Game {
     this.lightingManager.addToSceneRenderer(this.sceneRenderer);
 
     this.inputManager = new InputManager();
-    this.timer = new Timer();
 
     this.ground = new Ground();
     this.sceneRenderer.add(this.ground.getObject3D());
@@ -47,6 +51,10 @@ export default class Game {
     this.pauseHTMLElement = document.getElementById('pause') as HTMLElement;
     this.pauseHTMLElement.style.display = 'none';
 
+    this.setupKeydownListener();
+  }
+
+  private setupKeydownListener() {
     document.addEventListener('keydown', event => {
       if (event.key === 'r' || event.key === 'R') {
         this.reset();
@@ -55,6 +63,8 @@ export default class Game {
       }
     });
   }
+
+  private setupPause() {}
 
   public runGameLoop() {
     const deltaTime = this.timer.calculateDeltaTime();
